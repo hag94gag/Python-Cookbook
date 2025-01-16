e8adfcd60456d519dd3106a8d7ebce013f602bc0
hag94gag <hag94gag@gmail.com>
2025-01-16 16:49:16 +0200

    Added Comments

--- AutomateTheBoringStuffWithPython_AlSweigart/Basics/First_Program.py
+++ AutomateTheBoringStuffWithPython_AlSweigart/Basics/First_Program.py
@@ -1,8 +1,11 @@
 # This program says hello and asks for my name.
+
+#-------------------------------------------------------------------------------
 print("Hello")
-name=input("Enter your name:")
-print(f"Welcome {name} this is your first program")
-print(f"Your name length {len(name)}")
+name=input("Enter your name:") #take name as a string
+print(f"Welcome {name} this is your first program") #print the name
+print(f"Your name length {len(name)}") #print the length of the name
 age=int(input("What is your age:")) #take age as an intger
-print(f"You Will be {(age)+1} in a year")
+print(f"You Will be {(age)+1} in a year") #print the age after a year
+#-------------------------------------------------------------------------------
 
