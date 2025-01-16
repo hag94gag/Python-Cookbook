d887d0d711a9b63dda6d1274ef11f6c97c302edc
hag94gag <hag94gag@gmail.com>
2025-01-16 16:22:34 +0200

    First commit

---
+++ AutomateTheBoringStuffWithPython_AlSweigart/Basics/First_Program.py
@@ -0,0 +1,8 @@
+# This program says hello and asks for my name.
+print("Hello")
+name=input("Enter your name:")
+print(f"Welcome {name} this is your first program")
+print(f"Your name length {len(name)}")
+age=int(input("What is your age:")) #take age as an intger
+print(f"You Will be {(age)+1} in a year")
+
